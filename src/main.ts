a2e50b249fefb7ef4eecd8e1dd69b700eb830828
arsalan07 <m.arsalan@mythod.com>
2023-10-27 00:08:27 +0500

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.port || 3000
   // app.enableCors()
 
-  await app.listen(3000, "0.0.0.0");
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
